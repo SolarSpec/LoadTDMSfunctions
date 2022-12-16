1c9a876cd8c4f6deafc7b0ba033d62ed438c513f
dudeImpossible42 <hvidimlic1234@hotmail.com>
2022-12-15 19:39:24 -0800

    Fixed plotting multiple files

--- GeneralFunctions/SS_SelectFiles.m
+++ GeneralFunctions/SS_SelectFiles.m
@@ -1,16 +1,23 @@
-% Select certain files to import and plot
+%% Select certain files to import and plot. USUALLY SAME SIZE DATA
 clear % To ensure workspace refreshes each time script is run
+
+% Select the files and get number of files
 [FolderContent,path] = uigetfile('*.txt;*.csv;*.xlsx','Select one or more input files','MultiSelect','on');
 FolderContent = FolderContent';
-data = {};
-for FileIndex = 1:1:size(FolderContent,2)
+L = length(FolderContent);
+
+data = cell(L,1); % Preallocate cell array to hold each x and y column
+for FileIndex = 1:L
+        % Read X and Y data of each file
         data{FileIndex,1} = readmatrix(string(strjoin([path,FolderContent(FileIndex)],"")));
 
         hold on % This line allows to plot multiple lines on the same axis
         line = plot(data{FileIndex,1}(:,1),data{FileIndex,1}(:,2));
-        line.DisplayName = string(FolderContent(FileIndex)); % This assigns name of line object in legend
+
+        % This assigns name of line object in legend
+        line.DisplayName = string(FolderContent(FileIndex)); 
 end
 hold off % For consistency turn hold off
-legend("Box","off"); % You can change this to "on" for a box around legend
-xlim("tight") % You can change this to "padded" for some space around data
-ylim("tight") % You can change this to "padded" for some space around data
+legend("Box","off"); % You can change this to "on" for a box around your legend
+xlim("tight") % You can change this to "padded" for some space around your data
+ylim("tight") % You can change this to "padded" for some space around your data
